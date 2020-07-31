350d235822ea58b7be2ba039923329b1b547c800
Mary Mwihaki <marymwihaki16@gmail.com>
2020-07-31 16:42:17 +0300

    publish on github pages

---
+++ js/script.js
@@ -0,0 +1,237 @@
+/*DESIGN*/
+/*Click on image - show content, hide image*/
+
+$(document).ready(function(){
+  $('#imageDesign').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageDesign').on('click', function(){
+    $('#textDesign').hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageDesign').on('click', function(){
+    $('#contentDesign').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageDesign').on('click', function(){
+    $('#contentDes').show();
+  });
+});
+
+/*click on content heading - hide content, show image*/
+$(document).ready(function(){
+  $('#contentDesign').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDesign').on('click', function(){
+    $('#contentDes').hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDesign').on('click', function(){
+    $('#imageDesign').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDesign').on('click', function(){
+    $('#textDesign').show();
+  });
+});
+
+/*show image and heading by clicking on the description*/
+$(document).ready(function(){
+  $('#contentDes').on('click', function(){
+    $('#imageDesign').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDes').on('click', function(){
+    $('#textDesign').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDes').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDes').on('click', function(){
+    $('#contentDesign').hide();
+  });
+});
+
+/*DEVELOPMENT*/
+/*Click on image - show content, hide image*/
+
+$(document).ready(function(){
+  $('#imageDev').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageDev').on('click', function(){
+    $('#textDev').hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageDev').on('click', function(){
+    $('#contentDev').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageDev').on('click', function(){
+    $('#contentDevs').show();
+  });
+});
+
+/*click on content heading - hide content, show image*/
+$(document).ready(function(){
+  $('#contentDev').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDev').on('click', function(){
+    $('#contentDevs').hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDev').on('click', function(){
+    $('#imageDev').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDev').on('click', function(){
+    $('#textDev').show();
+  });
+});
+
+/*show image and heading by clicking on the description*/
+$(document).ready(function(){
+  $('#contentDevs').on('click', function(){
+    $('#imageDev').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDevs').on('click', function(){
+    $('#textDev').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDevs').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentDevs').on('click', function(){
+    $('#contentDev').hide();
+  });
+});
+
+/*PROJECT MANAGEMENT*/
+/*Click on image - show content, hide image*/
+
+$(document).ready(function(){
+  $('#imageProj').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageProj').on('click', function(){
+    $('#textProj').hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageProj').on('click', function(){
+    $('#contentProj').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#imageProj').on('click', function(){
+    $('#contentProjs').show();
+  });
+});
+
+/*click on content heading - hide content, show image*/
+$(document).ready(function(){
+  $('#contentProj').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentProj').on('click', function(){
+    $('#contentProjs').hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentProj').on('click', function(){
+    $('#imageProj').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentProj').on('click', function(){
+    $('#textProj').show();
+  });
+});
+
+/*show image and heading by clicking on the description*/
+$(document).ready(function(){
+  $('#contentProjs').on('click', function(){
+    $('#imageProj').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentProjs').on('click', function(){
+    $('#textProj').show();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentProjs').on('click', function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $('#contentProjs').on('click', function(){
+    $('#contentProj').hide();
+  });
+});
+
+/*HOVER*/
+$(document).ready(function(){
+  $('#work1').hover(function(){
+    $(this).css("color", "white");
+  });
+});
